64daeba1ead6399afe3c39a20403bfadc44e74f3
Luiz Pires - Ranzinza <luiz.pires@ranzinza.dev>
2023-02-10 16:56:27 -0300

    chore: add prisma orm

---
+++ prisma/migrations/20230210180833_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "phone" TEXT,
+    "date_of_birth" TEXT,
+    "photo_url" TEXT,
+    "address_street" TEXT,
+    "address_city" TEXT,
+    "address_state" TEXT,
+    "address_zip" TEXT,
+    "address_country" TEXT,
+    "is_verified" BOOLEAN NOT NULL DEFAULT false,
+    "is_active" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
