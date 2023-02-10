f4c23da98c280e5e5cfe3d09f095ebb9da6b8e7b
Luiz Pires - Ranzinza <luiz.pires@ranzinza.dev>
2023-02-10 18:03:40 -0300

    chore: add database error handling

---
+++ src/common/errors/interceptors/conflict-error.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  CallHandler,
+  ConflictException,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { catchError, Observable } from 'rxjs';
+import { ConflictError } from '../types/conflict-error';
+
+@Injectable()
+export class ConflictErrorInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError((error) => {
+        if (error instanceof ConflictError) {
+          throw new ConflictException(error.message);
+        } else {
+          throw error;
+        }
+      }),
+    );
+  }
+}
