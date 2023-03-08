014e740d930beaf25310a1624fe1bc665e780942
Luiz Pires - Ranzinza <luiz.pires@ranzinza.dev>
2023-03-08 15:33:29 -0300

    docs: add ApiProperty swagger

--- src/auth/dtos/auth.dto.ts
+++ src/auth/dtos/auth.dto.ts
@@ -1,9 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsStrongPassword } from 'class-validator';
 
 export class AuthDto {
+  @ApiProperty({
+    description: 'User email that will be used to login',
+    example: 'random@random.com',
+  })
   @IsEmail()
-  email: string;
+  public readonly email: string;
 
+  @ApiProperty({
+    description:
+      'The password must contain at least one uppercase letter, one special character and one number and be made up of at least 8 characters.',
+    example: 'R@nd0mP@ssw0rd',
+  })
   @IsStrongPassword()
   password: string;
 }
