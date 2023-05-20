199b27caa648fcaa64c69e37300aae7967d5e142
Luiz Pires - Ranzinza <luiz.pires@ranzinza.dev>
2023-05-20 13:32:36 -0300

    refactor: change database

---
+++ prisma/migrations/20230520162803_init/migration.sql
@@ -0,0 +1,62 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "full_name" TEXT NOT NULL,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "activated" BOOLEAN NOT NULL DEFAULT false,
+    "roles" JSONB,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_profile" (
+    "id" TEXT NOT NULL,
+    "phone" TEXT,
+    "date_of_birth" TEXT,
+    "photo_url" TEXT,
+    "address_street" TEXT,
+    "address_city" TEXT,
+    "address_state" TEXT,
+    "address_zip" TEXT,
+    "address_country" TEXT,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "user_profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserInternalId" (
+    "internal_id" SERIAL NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "UserInternalId_pkey" PRIMARY KEY ("internal_id")
+);
+
+-- CreateTable
+CREATE TABLE "EmailToken" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "EmailToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserInternalId_user_id_key" ON "UserInternalId"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "user_profile" ADD CONSTRAINT "user_profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "EmailToken" ADD CONSTRAINT "EmailToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
